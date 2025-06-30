1151614ef55f9d37e2d28fd3d773be2f6578c7e1
Nigel <84653162+ShigeoDev@users.noreply.github.com>
2025-06-29 22:41:11 -0400

    added speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import localFont from 'next/font/local';
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const LemonMilk = localFont({
   src: '../public/fonts/LEMONMILK-Regular.otf',
@@ -25,6 +26,7 @@ export default function RootLayout({
       >
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
